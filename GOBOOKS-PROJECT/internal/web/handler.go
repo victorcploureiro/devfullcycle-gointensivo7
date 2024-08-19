62ee8966bbdfcb07627f844f7652a23b35d0b246
Victor Capolo <victorcploureiro@gmail.com>
2024-08-19 15:31:19 -0300

    added CreateBooks handler

--- GOBOOKS-PROJECT/internal/web/handler.go
+++ GOBOOKS-PROJECT/internal/web/handler.go
@@ -20,3 +20,19 @@ func (h *BookHandlers) GetBooks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(books)
 }
+
+func (h *BookHandlers) CreateBook(w http.ResponseWriter, r *http.Request) {
+	var book service.Book
+	err := json.NewDecoder(r.Body).Decode(&book)
+	if err != nil {
+		http.Error(w, "invalid request payload", http.StatusBadRequest)
+		return
+	}
+	err = h.service.CreateBook(&book)
+	if err != nil {
+		http.Error(w, "failed to create book", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(book)
+}
