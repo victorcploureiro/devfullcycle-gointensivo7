31edafccde7e126dab5f77113701448ce63691c8
Victor Capolo <victorcploureiro@gmail.com>
2024-08-20 17:13:34 -0300

    maybe multithread?

--- GOBOOKS-PROJECT/cmd/exemplos/main.go
+++ GOBOOKS-PROJECT/cmd/exemplos/main.go
@@ -5,21 +5,19 @@ import (
 	"time"
 )
 
-func counter(n int) {
-	for i := range n {
-		fmt.Println(i)
+func worker(workerID int, data chan int) {
+	for x := range data {
+		fmt.Printf("worker %d got %d\n", workerID, x)
 		time.Sleep(time.Second)
 	}
 }
 
 func main() { // goroutine 1
-	ch := make(chan string) // empty
+	ch := make(chan int)
+	go worker(1, ch)
+	go worker(2, ch)
 
-	// goroutine 2
-	go func() {
-		ch <- "Full Cycle"
-	}()
-
-	msg := <- ch
-	fmt.Println(msg)
+	for i := range 10 {
+		ch <- i
+	}
 }
