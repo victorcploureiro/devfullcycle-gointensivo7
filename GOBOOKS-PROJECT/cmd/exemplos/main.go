97ab7b58c107c4fddf168a245b9a977ed44d2c85
Victor Capolo <victorcploureiro@gmail.com>
2024-08-20 16:37:54 -0300

    goroutines

---
+++ GOBOOKS-PROJECT/cmd/exemplos/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func counter(n int) {
+	for i := range n {
+		fmt.Println(i)
+		time.Sleep(time.Second)
+	}
+}
+
+func main() { // goroutine 1
+	ch := make(chan string) // empty
+
+	// goroutine 2
+	go func() {
+		ch <- "Full Cycle"
+	}()
+}
